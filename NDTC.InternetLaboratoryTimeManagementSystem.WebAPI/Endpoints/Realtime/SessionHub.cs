93af60e9dbad4f201e183bd8c2eb8fe13bc7caa7
Michael Angelo B. Zara <michaelangelobuccatzara@gmail.com>
2026-03-03 01:37:25 +0800

    fixed: signalr on disconnect event for loggin out yser

--- NDTC.InternetLaboratoryTimeManagementSystem.WebAPI/Endpoints/Realtime/SessionHub.cs
+++ NDTC.InternetLaboratoryTimeManagementSystem.WebAPI/Endpoints/Realtime/SessionHub.cs
@@ -1,17 +1,19 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using NDTC.InternetLaboratoryTimeManagementSystem.Application.Abstractions.Realtime.HubClients;
-using NDTC.InternetLaboratoryTimeManagementSystem.Application.Abstractions.Realtime.Services;
+using NDTC.InternetLaboratoryTimeManagementSystem.Application.Features.Commands.Accounts.Logout;
 
 namespace NDTC.InternetLaboratoryTimeManagementSystem.WebAPI.Endpoints.Realtime
 {
     [Authorize]
-    public class SessionHub(ISessionHubService sessionHubService)
+    public class SessionHub(ISender sender)
         : Hub<ISessionHubClient>
     {
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            await sessionHubService.PublishLoggedOutSessionOf();
+            var command = new LogoutCommand();
+            await sender.Send(command);
 
             await base.OnDisconnectedAsync(exception);
         }
